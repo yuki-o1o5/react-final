a4eb3bf4842c175dc7595e932fe81f3e51472314
yuki-o1o5 <jkksst1712@gmail.com>
2023-04-24 22:58:20 -0700

    docs/remove a reducer

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -2,17 +2,14 @@ import { CHANGE_COLORTHEME } from "../actions/colorThemeAction";
 import { darkTheme, lightTheme } from "../constants/themeColor";
 
 const initialState = {
-  color: "light",
   theme: lightTheme,
 };
 
 function rootReducer(state = initialState, action) {
   switch (action.type) {
     case CHANGE_COLORTHEME:
-      const currentColor = state.color === "light" ? "dark" : "light";
       const currentTheme = state.theme === lightTheme ? darkTheme : lightTheme;
       return {
-        color: currentColor,
         theme: currentTheme,
       };
     default: {
