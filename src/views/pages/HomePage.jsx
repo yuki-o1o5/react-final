04f50513ee58cb3a89eed177936c977b909e75db
yuki-o1o5 <jkksst1712@gmail.com>
2023-04-24 22:58:57 -0700

    docs/add modal

--- src/views/pages/HomePage.jsx
+++ src/views/pages/HomePage.jsx
@@ -1,5 +1,56 @@
-import React from "react";
+import { useState } from "react";
+
+import AddIcon from "@mui/icons-material/Add";
+import { Box, Modal, Typography, Fade, Backdrop } from "@mui/material";
 
 export default function HomePage() {
-  return <div>HomePage</div>;
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <h2>HomePage</h2>
+      <button onClick={handleOpen}>
+        <AddIcon />
+      </button>
+
+      <Modal
+        aria-labelledby="transition-modal-title"
+        aria-describedby="transition-modal-description"
+        open={open}
+        onClose={handleClose}
+        closeAfterTransition
+        slots={{ backdrop: Backdrop }}
+        slotProps={{
+          backdrop: {
+            timeout: 500,
+          },
+        }}
+      >
+        <Fade in={open}>
+          <Box sx={style}>
+            <Typography id="transition-modal-title" variant="h6" component="h2">
+              Text in a modal
+            </Typography>
+            <Typography id="transition-modal-description" sx={{ mt: 2 }}>
+              Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+            </Typography>
+          </Box>
+        </Fade>
+      </Modal>
+    </div>
+  );
 }
